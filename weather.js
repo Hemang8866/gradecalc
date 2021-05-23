425b733f386d9b9ab8e907a022eaf54b2a6c1bb0
Hemang8866 <hemangkoshiyar112@gmail.com>
2021-05-23 14:42:33 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
-const PORT = 3000;
+const PORT = process.env.PORT;
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
